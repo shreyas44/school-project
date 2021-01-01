55547a1f562ef4731b44855ec59ea982be0677d5
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-01-01 19:28:14 +0000

    Display all posts when logged in

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import redirect, render
+from datetime import datetime
 from users.models import User
+from .models import Post
 
 def home_view(request):
   if "user_id" not in request.session.keys():
     return redirect("login")
 
   user = User.objects.get(id=request.session["user_id"])
+  posts = Post.objects.all().order_by("-timestamp").select_related()
+  print(posts)
 
-  return render(request, "posts/home.html")
+  formatted_posts = []
+
+  for post in posts:
+    formatted_timestamp = datetime.utcfromtimestamp(post.timestamp).strftime('%-d %b %y')
+    formatted_post = post
+    formatted_post.timestamp = formatted_timestamp
+    formatted_posts.append(formatted_post)
+
+  data = {
+    "posts": formatted_posts,
+    "user": user,
+  }
+
+  return render(request, "posts/home.html", data)
