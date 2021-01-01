611b753aa0b342e357fd2754528ecd20284925aa
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-01-01 14:28:27 +0000

    Create post and user models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,34 @@
+from users.models import User
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+  body = models.CharField(max_length=10000)
+  timestamp = models.FloatField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = "Posts"
+
+class PostFile(models.Model):
+  url = models.CharField(max_length=1000)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = "PostFiles"
+
+class Like(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  timestamp = models.FloatField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = "Likes"
+
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  body = models.CharField(max_length=5000)
+  timestamp = models.FloatField()
+
+  class Meta:
+    db_table = "Comments"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  email = models.EmailField()
+
+  class Meta:
+    db_table = "Users"
