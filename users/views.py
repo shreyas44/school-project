954630836ac14afa3ce7ec9bdc1cc34103daba3a
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-01-01 17:03:38 +0000

    Get form data when login and signup forms are submitted using the POST method

--- users/views.py
+++ users/views.py
@@ -1,9 +1,26 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def login_view(request):
+  if request.method == "POST":
+    user_info = {
+      "username": request.POST.get("username"),
+      "password": request.POST.get("password")
+    }
+    print(user_info)
   return render(request, "users/login.html")
 
 def signup_view(request):
+  if request.method == "POST":
+    user_info = {
+      "first_name": request.POST.get("firstname"),
+      "last_name": request.POST.get("lastname"),
+      "username": request.POST.get("username"),
+      "password": request.POST.get("password"),
+      "repassword": request.POST.get("repassword"),
+    }
+
+    print(user_info)
   return render(request, "users/signup.html")
