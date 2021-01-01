3d6dc6eff94fe15219ee1ca4c5d13c2d523f7a3b
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-01-01 18:45:57 +0000

    Fix redirects

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,5 @@ from posts.views import home_view
 from django.urls import path
 
 urlpatterns = [
-  path("home", home_view) 
+  path("home", home_view, name="home") 
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from users.models import User
 
 def home_view(request):
-  if "user_id" in request.session.keys():
+  if "user_id" not in request.session.keys():
     return redirect("login")
 
   user = User.objects.get(id=request.session["user_id"])
