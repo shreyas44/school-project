e234006dc51ecdfb30a551d3fd41afa1806ee8fa
aditya303030 <73743156+aditya303030@users.noreply.github.com>
2021-01-02 17:39:23 +0530

    added a comma

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
   path("home", home_view, name="home"),
   path("toggleLike", csrf_exempt(toggle_like), name="toggle_like"),
-  path("newPost", csrf_exempt(new_post), name="new_post")
+  path("newPost", csrf_exempt(new_post), name="new_post"),
 ]
